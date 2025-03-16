2ccd5a4fb89f5bb1e38b999014f2f3ae2a671875
anastasiiaxfr <anastasiiaberest@gmail.com>
2025-03-16 11:09:57 +0200

    fix

--- src/pages/blog/constant.js
+++ src/pages/blog/constant.js
@@ -1,17 +1,19 @@
-export const countTopPost = 2
+export const countTopPost = 2;
+export const uri = 'blog';
+export const postPerPage = 6;
 
 export const allPosts = Object.values(
     import.meta.glob('../../md/blog/*.md', { eager: true })
-)
+);
 
 export const sortedPosts = allPosts.sort((a, b) => {
-    return new Date(b.frontmatter.pubdate) - new Date(a.frontmatter.pubdate)
-})
+    return new Date(b.frontmatter.pubdate) - new Date(a.frontmatter.pubdate);
+});
 
 export const allTags = Array.from(
     new Set(
         sortedPosts
-            .flatMap((service) => service.frontmatter.category || [])
-            .map((category) => category.trim())
+            .flatMap(service => service.frontmatter.category || [])
+            .map(category => category.trim())
     )
-)
+);
