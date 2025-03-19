3a7ce30b1b956156e06792e4ad37522d4417e7aa
Anastasia th9 <anastasiiaberest@gmail.com>
2025-03-19 15:05:55 +0200

    fix

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,20 +1,30 @@
 import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
+
+// Import all the markdown files under /src/md/blog/
+const blogPosts = import.meta.glob('/src/md/blog/*.md');
 
 export async function GET(context) {
-	// Fetch the blog collection from content
-	const blog = await getCollection('blog');
+	const posts = await Promise.all(
+		Object.keys(blogPosts).map(async (path) => {
+			const postModule = await blogPosts[path]();
+			return {
+				title: postModule.frontmatter.title,
+				pubDate: postModule.frontmatter.pubDate,
+				description: postModule.frontmatter.description,
+				link: `/blog/${postModule.frontmatter.slug}/`,
+				guid: `${context.site}/blog/${postModule.frontmatter.slug}/` // Unique guid
+			};
+		})
+	);
 
 	return rss({
 		title: 'FxSerm',
 		description: 'A humble Astronaut’s guide to the stars',
 		site: context.site,
-		items: blog.map((post) => ({
-			title: post.data.title, // Post title from the frontmatter
-			pubDate: post.data.pubDate, // Post publication date
-			description: post.data.description, // Post description from frontmatter
-			link: `/blog/${post.id}/` // Link to the individual blog post
-		})),
-		customData: `<language>en-us</language>` // Custom XML data
+		items: posts,
+		customData: `
+      <language>en-us</language>
+      <atom:link xmlns:atom="http://www.w3.org/2005/Atom" href="${context.site}/rss.xml" rel="self" />
+    `
 	});
 }
